609645eb8250a0deaf91c64f02464165202fa8d9
zkwjq <zkwjq@sunnyoptical.com>
2020-05-21 12:41:46 +0800

    kingkinotes

---
+++ src/main/java/com/kingki/studynotes/socket/client/Client.java
@@ -0,0 +1,74 @@
+package com.kingki.studynotes.socket.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client extends Thread{
+
+    Socket socket = null;
+
+    public Client (String host,int port){
+        try {
+            socket = new Socket(host,port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void run() {
+        new sendMessThread().start();
+        super.run();
+        try {
+            // 读Sock里面的数据
+            InputStream s = socket.getInputStream();
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while ((len = s.read(buf)) != -1) {
+                System.out.println(new String(buf, 0, len));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class sendMessThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            //写操作
+            Scanner scanner=null;
+            OutputStream os= null;
+            try {
+                scanner=new Scanner(System.in);
+                os= socket.getOutputStream();
+                String in="";
+                do {
+                    in=scanner.next();
+                    os.write((""+in).getBytes());
+                    os.flush();
+                } while (!in.equals("bye"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            scanner.close();
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client clientTest=new Client("127.0.0.1", 1234);
+        clientTest.start();
+    }
+}
+
+
